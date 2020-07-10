698867e40588224d1bb91f30eeea136c1f118fa3
austinlhx <aho103@syr.edu>
2020-07-09 21:41:25 -0700

    Fixed golint issue

--- server/models/product.go
+++ server/models/product.go
@@ -1,13 +1,13 @@
 package models
 
-//Product sets the product being recieved from a post request
+//Product sets the product being received from a post request
 type Product struct {
 	ProductName string `json:"ProductName, omitempty"`
 	ProductType string `json:"ProductType, omitempty"`
 	UpperBound  string `json:"UpperBound, omitempty"`
 	LowerBound  string `json:"LowerBound, omitempty"`
 }
-//Products sets the product being recieved from a post request
+//Products sets the product being received from a post request
 type Products struct {
 	ProductName string
 	ProductType string
@@ -15,7 +15,7 @@ type Products struct {
 	LowerBound  float64
 }
 
-//ProductFound sets the product being recieved from a post request
+//ProductFound sets the product being received from a post request
 type ProductFound struct {
 	Name string 
 	Price float64
